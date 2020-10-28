79fb190086073df01055665b8e95ecabcd47a7a2
sir-teagus <teagus@gmail.com>
2020-10-28 19:55:59 -0300

    Correct the http response

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { v4: uuid, validate: isUuid, v4 } = require("uuid");
+const { v4: uuid, validate: isUuid } = require("uuid");
 
 const app = express();
 
@@ -20,25 +20,29 @@ function validateRepositoryId(request, response, next) {
   return next();
 }
 
+app.use("/repositories/:id", validateRepositoryId);
+
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs, likes } = request.body;
+  const { title, url, techs } = request.body;
 
-  const repository = { id: v4(), title, url, techs, likes };
-
-  if (!isUuid(repository.id) || repository.likes != 0) {
-    return response.status(400).json({ error: "Invalid data entry." });
-  }
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
 
   repositories.push(repository);
 
-  return response.json({ repository });
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", validateRepositoryId, (request, response) => {
+app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body;
 
@@ -46,7 +50,7 @@ app.put("/repositories/:id", validateRepositoryId, (request, response) => {
     (repository) => repository.id == id
   );
 
-  if (repositoryIndex < 0) {
+  if (repositoryIndex == -1) {
     return response.status(400).json({ error: "ID not found." });
   }
 
@@ -60,17 +64,17 @@ app.put("/repositories/:id", validateRepositoryId, (request, response) => {
 
   repositories[repositoryIndex] = repository;
 
-  return response.json({ repository });
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", validateRepositoryId, (request, response) => {
+app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
   const repositoryIndex = repositories.findIndex(
     (repository) => repository.id == id
   );
 
-  if (repositoryIndex < 0) {
+  if (repositoryIndex == -1) {
     return response.status(400).json({ error: "ID not found." });
   }
 
